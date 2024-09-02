f03db9560fd916b9b9fae7cde208dc43d21a0df3
Gaurav Sahu <124136957+gauravsahuuu@users.noreply.github.com>
2024-09-02 20:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the models
 with open("diabetese_trained_model.pkl", 'rb') as f:
